ebfef6262d92b896307ac4dca5d50cd6d29f632a
Marco <marco.carone@libero.it>
2020-03-10 20:16:33 +0100

    fix iva

--- traits/PrezzoIvato.php
+++ traits/PrezzoIvato.php
@@ -2,11 +2,11 @@
 
 trait PrezzoIvato
 {
-  
+
   public $prezzoIvato;
 
   public function prezzoIvato($iva){
-     $prezzoconIva = $this->prezzo - (($this->prezzo * $iva) / 100);
+     $prezzoconIva = $this->prezzo + (($this->prezzo * $iva) / 100);
      return $this->prezzoIvato = $prezzoconIva;
   }
 }
