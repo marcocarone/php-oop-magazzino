41cbc379645634d9ca5d475458b46690d48c2d58
Marco <marco.carone@libero.it>
2020-03-10 15:59:02 +0100

    continuo

--- classi/Fotocamera.php
+++ classi/Fotocamera.php
@@ -3,14 +3,16 @@ require_once  "Prodotto.php";
 
 class Fotocamera extends Prodotto
 {
-  public $mirino;
+  public $messaFuoco;
   public $sensore;
   public $ISO;
 
   public function __construct($_sku, $_marca, $_modello, $_prezzo, $_sensore)
   {
     parent::__construct($_sku, $_marca, $_modello, $_prezzo)
+    $this->messaFuoco = $messaFuoco;
     $this->sensore = $_sensore;
+    $this->ISO = $ISO;
   }
 }
 
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+//includo le classi che mi servono
+require_once "classi/Prodotto.php";
+require_once "classi/Fotocamera.php";
+
+// creo una istanza nuova
+$fotocamera = new Fotocamera("K0345", "Canon", "EOS M50", 350, "CMOS da 22,3 x 14,9 mm")
+ ?>
